c87ae47a07928e4b81e6c26c0ff8ee72cec29d86
Marijn van der Zee <marijn@serraict.com>
2017-05-29 20:26:02 +0200

    Test second page too

--- tests/wikidoc_self.py
+++ tests/wikidoc_self.py
@@ -12,11 +12,12 @@ _out_dir = build_docs.OUT_DIR
 class OwnWikiToDocTestSuite(unittest.TestCase):
     """Basic test cases."""
 
-    def test_write_pdf(self):
+    def test_own_wikidoc(self):
         shutil.rmtree(_out_dir, ignore_errors=True)
         build_docs.build_docs()
         self.assert_exists('index.html')
         self.assert_exists('Home')
+        self.assert_exists('Another-page')
 
     def assert_exists(self, rel_path):
         p = os.path.join(_out_dir, rel_path)
